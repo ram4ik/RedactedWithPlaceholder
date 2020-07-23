af32fe31041c15191b759fa810241151e3dd8445
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-23 16:37:02 +0300

    Initial Commit

---
+++ RedactedWithPlaceholder/RedactedWithPlaceholderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedactedWithPlaceholderApp.swift
+//  RedactedWithPlaceholder
+//
+//  Created by ramil on 23.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct RedactedWithPlaceholderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
